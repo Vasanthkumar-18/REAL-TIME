ccff4c12210b0812ddbb1fd954fd859a9db7e54d
Vasanthkumar-18 <vasanthdukelover143@gmail.com>
2025-01-10 23:22:23 -0800

    Added Time code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState }  from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+ setInterval (ClickTime,1000)
+
+  const gettime = new Date().toLocaleTimeString();
+
+  const [time , SetTime ] = useState(gettime)
+
+  function ClickTime() {
+ 
+    const newTime = new Date().toLocaleTimeString();
+
+    return (
+
+    SetTime(newTime )
+    )
+
+    
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='time'>
+      <h1>
+        {time}
+      </h1>
+
+    </div>
   )
+
 }
 
 export default App
